4de3e0454e005e6f37cc1de198caee6c67e3944b
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-09-19 16:02:55 -0300

    Add files via upload

---
+++ lucas_210918.py
@@ -0,0 +1,22 @@
+print("Question 2")
+class Zuz():
+    def __init__(self):
+        print("Lucas")
+print("Question 3")
+tasd=Zuz()
+print("Question 4")
+tasd.ocd="The Lucas"
+print("Question 5")
+print(tasd.ocd)
+print("Question 6")
+class Aua():
+    def __init__(self):
+        print("Lucas")
+    def name(self):
+        self.nm=12
+print("Question 7")
+asdt=Aua()
+print("Question 8")
+asdt.name()
+print("Question 9")
+print(asdt.nm)
